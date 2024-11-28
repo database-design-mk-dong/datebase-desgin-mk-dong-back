c46eb871f9255078b34e9bb91adf610d6952adcd
park20011029 <rokmck156@gmail.com>
2024-11-28 22:56:41 +0900

    Fix: cors수정

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -29,5 +30,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(this.userIdInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns(Constants.NO_NEED_AUTH_URLS);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET","POST","PUT","DELETE","HEAD","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
